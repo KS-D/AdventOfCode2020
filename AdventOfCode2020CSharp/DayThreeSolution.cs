cf3138c4723f3b6f258c7245d1eb20cda0b3be59
KS-D <tikiph@gmail.com>
2020-12-04 19:41:55 -0800

    Finished the 3rd challenge

--- AdventOfCode2020CSharp/DayThreeSolution.cs
+++ AdventOfCode2020CSharp/DayThreeSolution.cs
@@ -1,12 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace AdventOfCode2020CSharp
 {
     class DayThreeSolution
     {
+        public List<char[]> TestSlope = new List<char[]> 
+        { 
+            new [] { '.','.', '#', '#', '.', '.', '.', '.', '.', '.', '.' }, 
+            new [] { '#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '.' },
+            new [] { '.', '#', '.', '.', '.', '.', '#', '.', '.', '#', '.' },
+            new [] { '.', '.', '#', '.', '#', '.', '.', '.', '#', '.', '#' },
+            new [] { '.', '#', '.', '.', '.', '#', '#', '.', '.', '#', '.' },
+            new [] { '.', '.', '#', '.', '#', '#', '.', '.', '.', '.', '.' },
+            new [] { '.', '#', '.', '#', '.', '#', '.', '.', '.', '.', '#' },
+            new [] { '.', '#', '.', '.', '.', '.', '.', '.', '.', '.', '#' },
+            new [] {'#', '.', '#', '#', '.', '.', '.', '#', '.', '.', '.' },
+            new [] {'#', '.', '.', '.', '#', '#', '.', '.', '.', '.', '#' },
+            new [] {'.', '#', '.', '.', '#', '.', '.', '.', '#', '.', '#' }
+        };
+
+        public List<char[]> GetInput()
+        {
+            StreamReader sr = new("day3.txt");
+            List<char[]> slope = new();
+
+            while (!sr.EndOfStream)
+            {
+                string input = sr.ReadLine();
+                char[] rowOfSlope = input.ToCharArray();
+
+                slope.Add(rowOfSlope);
+            }
+
+            return slope;
+        }
+
+
+        public int SolveSlopeProblem(List<char[]> slope, int downChecks = 1, int rightChecks = 3)
+        {
+            int count = 0;
+            
+            //PrintLine(slope[0]);
+
+            for (int i = downChecks, j = rightChecks; i < slope.Count; i+=downChecks, j+=rightChecks)
+            {
+                if (j > slope[i].Length - 1)
+                {
+                    j -= slope[i].Length;
+                }
+                
+                if (slope[i][j] == '#')
+                {
+                   //slope[i][j] = 'X';
+                   ++count;
+                }
+                //else
+                //{
+                //    slope[i][j] = 'O';
+                //}
+                
+                //PrintLine(slope[i]);
+            }
+
+            return count;
+        }
+
+        private void PrintLine(char[] line)
+        {
+            foreach (var c in line)
+            {
+               Console.Write(c); 
+            }
+            Console.WriteLine();
+        }
     }
 }
--- AdventOfCode2020CSharp/Program.cs
+++ AdventOfCode2020CSharp/Program.cs
@@ -7,14 +7,46 @@ namespace AdventOfCode2020CSharp
     {
         static void Main(string[] args)
         {
-            DayTwoSolutions sol2;
-            List<PasswordRequirement> passwordReqs = sol2.GetInput();
+            DayThreeSolution sol3 = new();
 
-            sol2.SolveValidPassword(passwordReqs);
-            Console.WriteLine("The correct Answer is 572");
+            List<char[]> slopes = sol3.GetInput();
 
-            sol2.SolveValidPassword2(passwordReqs);
-            Console.WriteLine("The correct Answer is 306");
+            Console.WriteLine("First Scenario");
+            int count = sol3.SolveSlopeProblem(slopes);
+            Console.WriteLine("Second Scenario");
+            int count2 = sol3.SolveSlopeProblem(slopes, 1, 1);
+            Console.WriteLine("Third Scenario");
+            int count3 = sol3.SolveSlopeProblem(slopes, 1, 5);
+            Console.WriteLine("Fourth Scenario");
+            int count4 = sol3.SolveSlopeProblem(slopes, 1, 7);
+            Console.WriteLine("Fifth Scenario");
+            int count5 = sol3.SolveSlopeProblem(slopes, 2, 1);
+
+            int[] collisions = { count, count2, count3, count4, count5 }; 
+            
+            Console.WriteLine($"my count is: {count}");
+            Console.WriteLine($"my count is: {count2}");
+            Console.WriteLine($"my count is: {count3}");
+            Console.WriteLine($"my count is: {count4}");
+            Console.WriteLine($"my count is: {count5}");
+
+            long product = 1;
+
+            foreach (int i in collisions)
+            {
+                if (i != 0)
+                {
+                    product *= i;
+                }
+            }
+
+            Console.WriteLine("\nFirst Answer: ");
+            Console.WriteLine($"my count is: {count}");
+            Console.WriteLine($"The correct count is: {181}");
+
+            Console.WriteLine("\nSecond Answer: ");
+            Console.WriteLine($"The product is: {product}");
+            Console.WriteLine($"The Correct Answer is: {1260601650}");
         }
     }
 }
