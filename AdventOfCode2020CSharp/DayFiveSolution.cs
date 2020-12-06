9d246860492a51adfbcc8affadcbc5cb54a32efc
KS-D <tikiph@gmail.com>
2020-12-06 00:56:02 -0800

    finished solution for day 5, might clean it up a bit

---
+++ AdventOfCode2020CSharp/DayFiveSolution.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020CSharp
+{
+    class DayFiveSolution
+    {
+        private List<string> GetInput()
+        {
+            List<string> seatRules = new();
+            StreamReader sr = new("day5.txt");
+
+            while (!sr.EndOfStream)
+            {
+                string seats = sr.ReadLine();
+                if (seats is not null)
+                {
+                    seatRules.Add(seats);
+                }
+            }
+            
+            return seatRules;
+        }
+        
+        public (int row, int col) SolveSeatProblem(string seatRule)
+        {
+            int frontEdge = 0;
+            int backEdge = 127;
+            char[] rules = seatRule.ToCharArray();
+
+            int leftEdge = 0;
+            int rightEdge = 7;
+
+            foreach (char rule in rules)
+            {
+                var midDist = 0;
+                if (rule == 'F')
+                {
+                    midDist = (backEdge - frontEdge)/2;
+                    backEdge -= midDist + 1;
+                } 
+                else if (rule == 'B')
+                {
+                    midDist = (backEdge - frontEdge)/2;
+                    ++midDist;
+                    frontEdge += midDist;
+                }
+                else
+                {
+                    var midColumn = 7;
+                    if (rule == 'R')
+                    {
+                        midColumn = (rightEdge - leftEdge) / 2;
+                        leftEdge += midColumn + 1;
+                    }
+                    else if (rule == 'L')
+                    {
+                        midColumn = (rightEdge - leftEdge) / 2;
+                        rightEdge -= midColumn + 1;
+                    }
+                }
+            }
+
+            if (frontEdge == backEdge && leftEdge == rightEdge)
+            {
+                return (frontEdge, leftEdge);
+            }
+            else
+            {
+                throw new ArithmeticException("A seat was not successfully found");
+            }
+        }
+
+        public int CalculateSeatId(int row, int col)
+        {
+            return row * 8 + col;
+        }
+
+        public int FindHighestSeatId()
+        {
+            List<string> seatRule = GetInput();
+            int highest = 0;
+
+            foreach (string seat in seatRule)
+            {
+                (int row, int column) = SolveSeatProblem(seat);
+                int temp = CalculateSeatId(row, column);
+                if (temp > highest)
+                {
+                    highest = temp;
+                }
+            }
+            return highest;
+        }
+
+        // return a sorted list of ids
+        public List<int> GetAllSeatIds()
+        {
+            List<int> ids = new();
+            List<string> seatRule = GetInput();
+            
+            foreach (string seat in seatRule)
+            {
+                (int row, int column) = SolveSeatProblem(seat);
+                int id = CalculateSeatId(row, column);
+                ids.Add(id);
+            }
+            
+            ids.Sort();
+            return ids;
+        }
+
+        // return negative -1 if not found
+        public int FindMySeat(List<int> ids)
+        {
+            for (int i = 0, j = 1, k = 2; k < ids.Count; ++i, ++j, ++k)
+            {
+                if (ids[i] != ids[j] - 1) // difference of greater than 1 means a missing seat;
+                {
+                    return ids[j] - 1;
+                }
+            }
+            
+            return -1;
+        }
+    }
+}
--- AdventOfCode2020CSharp/Program.cs
+++ AdventOfCode2020CSharp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020CSharp
 {
@@ -8,23 +7,16 @@ namespace AdventOfCode2020CSharp
     {
         static void Main(string[] args)
         {
-            DayFourSolutions sol4 = new ();
-            
-            var input = sol4.GetInput();
+            DayFiveSolution sol5 = new();
            
-            Regex rx = new (@$"(?=.*byr\:)(?=.*iyr\:)(?=.*eyr\:)(?=.*hgt\:)(?=.*hcl\:)(?=.*ecl\:)(?=.*pid\:)");
-            int count = sol4.GetValidPassports(input, rx);
-          
-            //Correct answer for part 1 is 242 
-            Console.WriteLine($"The count of valid passports is: { count }");
-            
-            Regex rx2 = new(@"(?=.*byr\:((19)[2-9][0-9]|(200)[0-2]))(?=.*iyr\:((201)[0-9]|(2020)))" +
-                                   @"(?=.*eyr\:((202)[0-9]|2030))(?=.*hgt\:(((1)[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in))" +
-                                   @"(?=.*hcl\:#[0-9a-fA-F]{6})(?=.*ecl\:(amb|blu|brn|gry|grn|hzl|oth))(?=.*pid\:[0-9]{9}\b)");
+            int highestSeatId = sol5.FindHighestSeatId();
+            Console.WriteLine($"Highest Seat Id: {highestSeatId}");
 
-            count = sol4.GetValidPassports(input, rx2);
-            Console.WriteLine($"The count of valid passports is: { count }");
-            //Correct answer is 186
-       }
+            List<int> ids = sol5.GetAllSeatIds();
+            int mySeat = sol5.FindMySeat(ids);
+            Console.WriteLine($"My Seat is:{mySeat}");
+            
+            
+        }
     }
 }
