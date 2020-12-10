9ab9de970f87e734f08f759b2f36ba7a7b9a8dcb
KS-D <tikiph@gmail.com>
2020-12-09 17:33:25 -0800

    got a solution! might clean it up later

---
+++ AdventOfCode2020CSharp/DaySevenSolution.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020CSharp
+{
+    class DaySevenSolution
+    {
+        public List<string> GetInput(string inputFile)
+        {
+            List<string> baggageRules = new();
+            using StreamReader sr = new(inputFile);
+
+            while (!sr.EndOfStream)
+            {
+                string rule = sr.ReadLine();
+                if (!string.IsNullOrWhiteSpace(rule))
+                {
+                    baggageRules.Add(rule);
+                }
+            }
+            return baggageRules;
+        }
+
+        public Dictionary<string, Dictionary<string, int>> BagHolds(List<string> bagRules)
+        {
+            Dictionary<string, Dictionary<string, int>> bagHolds = new();
+            string[] splitRules = {"contain", ","};
+            Regex r = new(@"[0-9]+");
+            
+            foreach (var s in bagRules)
+            {
+                var noBag = s.Trim().Replace("bags", "")
+                                          .Replace("bag","")
+                                          .Replace(".", "");
+                
+                var parsed = noBag.Split(splitRules, StringSplitOptions.None)
+                                          .Select(bag => bag.Trim()) 
+                                          .ToArray();
+                string key = "";
+                foreach (var t in parsed)
+                {
+                    if (t.Contains("no other"))
+                    {
+                        // don't add anything
+                    } 
+                    else if (!r.IsMatch(t))                     
+                    {
+                        key = t;
+                        bagHolds.Add(key, new());
+                    }
+                    else
+                    {
+                        var match = r.Match(t);
+                        var numOfBags = int.Parse(match.Value);
+                        var substringIndex = match.Index + match.Length + 1; // add 1 to remove the space
+                        var bagType = t.Substring(substringIndex);
+                        bagHolds[key].Add(bagType, numOfBags);
+                    }
+                }
+            }
+            return bagHolds;
+        }
+
+        public Dictionary<string, List<string>> BagsHeldBy(Dictionary<string, Dictionary<string, int>> bagsHold)
+        {
+            Dictionary<string, List<string>> heldBy = new();
+            foreach (var bag in bagsHold)
+            {
+                foreach (var heldBag in bag.Value)
+                {
+                    if (!heldBy.ContainsKey(heldBag.Key))
+                    {
+                        heldBy.Add(heldBag.Key,new());
+                    }
+                    heldBy[heldBag.Key].Add(bag.Key);
+                }
+            }
+            return heldBy;
+        }
+
+        public HashSet<string> PathsToColor(string bagColor,Dictionary<string, List<string>> bagHeldBy)
+        {
+            if (bagHeldBy.ContainsKey(bagColor))
+            {
+                HashSet<string> colors = bagHeldBy[bagColor].ToHashSet();
+                foreach (var bag in bagHeldBy[bagColor])
+                {
+                    colors.UnionWith(PathsToColor(bag, bagHeldBy));
+                }
+                return colors;
+            }
+            return new();
+        }
+
+        public int BagsInside(string bagColor, Dictionary<string, Dictionary<string, int>> bagHolds)
+        {
+            if (bagHolds.ContainsKey(bagColor))
+            {
+                int sum = 0;
+                foreach (var pair in bagHolds[bagColor])
+                {
+                    sum += pair.Value;
+                    sum += pair.Value * BagsInside(pair.Key, bagHolds);
+                }
+
+                return sum;
+            }
+            return 0;
+        }
+    } 
+}
--- AdventOfCode2020CSharp/Program.cs
+++ AdventOfCode2020CSharp/Program.cs
@@ -7,14 +7,18 @@ namespace AdventOfCode2020CSharp
     {
         static void Main(string[] args)
         {
-            DaySixSolution sol6 = new();
-
-            List<string> questions = sol6.GetQuestionInput();
+            DaySevenSolution sol7 = new();
             
-            // 7128 is the correct answer
-            Console.WriteLine($"Here are the unique answers: {sol6.SolveTotalQuestions(questions)}");
-            // 3640 is to high
-            Console.WriteLine($"Here are the shared group totaled: {sol6.SolveDuplicateQuestions(questions)}");
+            List<string> bagRules = sol7.GetInput("day7.txt");
+            var bagsHold = sol7.BagHolds(bagRules);
+            var heldBag = sol7.BagsHeldBy(bagsHold);
+            
+            var set = sol7.PathsToColor("shiny gold", heldBag);
+            // answer was 326
+            Console.WriteLine($"Count: {set.Count}");
+            // answer was 5635
+            int bagsInside = sol7.BagsInside("shiny gold", bagsHold);
+            Console.WriteLine($"Bags inside of shiny gold: {bagsInside}");
         }
     }
 }
