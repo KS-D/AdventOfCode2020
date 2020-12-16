f8a4a8168e2bd091a59d31fc49244b78126e143e
KS-D <tikiph@gmail.com>
2020-12-15 22:42:20 -0800

    solution for day 15

---
+++ AdventOfCode2020CSharp/DayFifteenSolution.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020CSharp
+{
+    class DayFifteenSolution
+    {
+
+        public int NumberGame(string input, int range = 2020)
+        {
+            Dictionary<int, Stack<int>> valueAndTurn = new();
+            List<int> parsed = input.Split(",").Select(int.Parse).ToList();
+            for (int i = 0; i < parsed.Count; i++)
+            {
+                valueAndTurn.Add(parsed[i], new Stack<int>());
+                valueAndTurn[parsed[i]].Push(i + 1);
+            }
+            
+            int count = valueAndTurn.Count + 1;
+            int lastKey = valueAndTurn.Last().Key;
+            while (count <= range)
+            {
+                var lastValue = valueAndTurn[lastKey];
+                if (lastValue.Count > 1)
+                {
+                    int newestIndex = lastValue.Pop();
+                    int oldestIndex = lastValue.Pop();
+                    lastValue.Push(newestIndex);
+
+                    lastKey = newestIndex - oldestIndex;
+                }
+                else
+                {
+                    lastKey = 0;
+                }
+                
+                if (valueAndTurn.ContainsKey(lastKey))
+                { 
+                    valueAndTurn[lastKey].Push(count);
+                }
+                else
+                { 
+                    valueAndTurn.Add(lastKey, new Stack<int>()); 
+                    valueAndTurn[lastKey].Push(count);
+                }
+
+                count++;
+            }
+             
+            return lastKey;
+        }
+    }
+}
--- AdventOfCode2020CSharp/Program.cs
+++ AdventOfCode2020CSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace AdventOfCode2020CSharp
 {
@@ -7,14 +8,31 @@ namespace AdventOfCode2020CSharp
     {
         static void Main(string[] args)
         {
-            DayThirteenSolution sol13 = new();
-
-            //sol13.GetInput("day13_test.txt");
-            sol13.GetInput("day13.txt");
-            Console.WriteLine($"{sol13.FindRemainder()}");
+            DayFifteenSolution sol15 = new();
             
-            //
-            Console.WriteLine(sol13.FindTimeStampCRTCon());
+            // test data result part 1
+            Console.WriteLine(sol15.NumberGame("0,3,6"));
+            // part 1 
+            // correct answer 1085
+            Console.WriteLine(sol15.NumberGame("1,20,11,6,12,0"));
+            
+            //part 2 test data
+            var watch = new Stopwatch();
+            watch.Start();
+            var lastNumber = sol15.NumberGame("0,3,6", 30000000);
+            watch.Stop();
+            Console.WriteLine(lastNumber);
+            Console.WriteLine($"Time elapsed: {watch.Elapsed.Milliseconds} ms");
+ 
+            watch.Reset();
+            watch.Start();
+            lastNumber = sol15.NumberGame("1,20,11,6,12,0", 30000000);
+            watch.Stop();
+            // part 2 answer
+            Console.WriteLine(lastNumber);
+            
+            Console.WriteLine($"Time elapsed: {watch.Elapsed.Milliseconds} ms");
+           
         }
     }
 }
